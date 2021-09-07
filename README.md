3968e2ec08486172513e184bc7899479d096dcbd
Jim Rasche <jjrasche@umich.edu>
2021-09-07 13:37:31 -0400

    canvas on the screen.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kingdom
 Recreation of the phone game Antiyoy for the browser, with the goal to train reinforcement learning algorithm on it.
+
+## startup
+`npm init`
+`npx tsc --init`
---
+++ build/index.js
@@ -0,0 +1,25 @@
+export let CANVAS;
+export let CTX;
+function startGame() {
+    // test
+    var t = 5;
+    initCanvas();
+    setCanvasDimensions();
+    CTX.beginPath(); // Start a new path
+    CTX.moveTo(30, 50); // Move the pen to (30, 50)
+    CTX.lineTo(150, 100); // Draw a line to (150, 100)
+    CTX.stroke(); // Render the path
+}
+function initCanvas() {
+    CANVAS = document.getElementById("canvas");
+    CTX = CANVAS.getContext("2d") ?? new CanvasRenderingContext2D();
+    CTX.imageSmoothingEnabled = false;
+}
+function setCanvasDimensions() {
+    CANVAS.style.width = `${window.innerWidth}px`;
+    CANVAS.style.height = `${window.innerHeight}px`;
+    CANVAS.width = Math.floor(window.innerWidth * window.devicePixelRatio);
+    CANVAS.height = Math.floor(window.innerHeight * window.devicePixelRatio);
+}
+startGame();
+//# sourceMappingURL=index.js.map
